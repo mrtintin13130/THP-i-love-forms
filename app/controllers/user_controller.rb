e9942dfa41ca53418c9033b873e9d6ecb76779ad
Martin <mts13@hotmail.fr>
2018-10-30 18:44:06 +0100

    un peu de css

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,7 +3,11 @@ class UserController < ApplicationController
   end
 
   def create
-  	user = User.new(username: params[:username], email: params[:email], bio: params[:bio])
-    user.save
+  	@user = User.new('username' => params[:username],
+                   'email' => params[:email],
+                   'bio' => params[:bio])
+  	@user.save
+  	# user = User.new(username: params[:username], email: params[:email], bio: params[:bio])
+    # user.save
   end
 end
