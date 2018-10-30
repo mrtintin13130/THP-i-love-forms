83b084b354c6a8e17a858d2a463c3bcf2e57c0f6
Martin <mts13@hotmail.fr>
2018-10-31 00:02:06 +0100

    modif

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -10,4 +10,8 @@ class UserController < ApplicationController
   	# user = User.new(username: params[:username], email: params[:email], bio: params[:bio])
     # user.save
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get '/', to: 'index#index'
   get 'user/new', to: 'user#new'
   post 'user/new', to: 'user#create'
+  get 'user/show/:id', to: 'user#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
